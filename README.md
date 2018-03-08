e2ce63fbb660ca518c02afc27e0aae727f86c03e
dennischou <dennischou@intumit.com>
2018-03-08 22:20:19 +0800

    still can't work

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 practice
 
 # Reference
+<https://spring.io/guides/gs/spring-boot/#scratch>
 <http://blog.didispace.com/spring-boot-learning-1/>
+<https://www.petrikainulainen.net/programming/gradle/getting-started-with-gradle-creating-a-spring-boot-web-application-project/>
+<https://www.ibm.com/developerworks/cn/java/j-spring-boot-basics-perry/index.html>
---
+++ source/spring-practice/src/main/java/dennis/practice/spring/HelloController.java
@@ -0,0 +1,18 @@
+package dennis.practice.spring;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+	
+    @RequestMapping("/hello/${name}")
+    public String helloWithName(@PathVariable String name) {
+        return "Hello " + name;
+    }
+}
